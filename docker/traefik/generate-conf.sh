37bf45e4f7ae3245b0d8a240c218d0641d968a28
kevinand11 <kevinfizu@gmail.com>
2023-03-08 15:22:00 +0100

    refactor: build traefik config

--- docker/traefik/generate-conf.sh
+++ docker/traefik/generate-conf.sh
@@ -1,45 +1,13 @@
 #!/bin/sh
 
-CONF_PATH=/etc/traefik/traefik.yml
-
-CERT_TYPE=production
-
 CERT_PATH_STAGING=/etc/traefik/acmeStaging.json
 CERT_PATH_PRODUCTION=/etc/traefik/acmeProduction.json
+CERT_TYPE=production
 
-if [ "$USE_SSL" = 1 ]; then
-cat > $CONF_PATH <<- EOF
+similar="
 global:
   checkNewVersion: true
   sendAnonymousUsage: false
-
-entryPoints:
-  web:
-    address: :80
-
-  websecure:
-    address: :443
-
-http:
-  middlewares:
-    stripRoutePrefix:
-      stripPrefix:
-        prefixes:
-          - "/api"
-  routers:
-    api:
-      tls:
-        certresolver: $CERT_TYPE
-      rule: "Host(\`$BASE_DOMAIN\`) && PathPrefix(\`/api/\`)"
-      middlewares:
-        - stripRoutePrefix
-      service: api
-
-  services:
-    api:
-      loadBalancer:
-        servers:
-          - url: http://api:$PORT/
 
 api:
   insecure: true
@@ -50,12 +18,51 @@ providers:
     directory: /etc/traefik
     watch: true
 
+http:
+  middlewares:
+    stripRoutePrefix:
+      stripPrefix:
+        prefixes:
+          - \"/api\"
+
+  services:
+    api:
+      loadBalancer:
+        servers:
+          - url: http://api:$PORT/
+
+  routers:
+    api:
+      middlewares:
+        - stripRoutePrefix
+      rule: \"Host(\`$BASE_DOMAIN\`) && PathPrefix(\`/api/\`)\"
+      service: api"
+
+routers=""
+entryPoints="
+entryPoints:
+  web:
+    address: :80
+"
+certResolvers=""
+
+if [ "$USE_SSL" = 1 ]; then
+entryPoints="$entryPoints
+  websecure:
+    address: :443
+"
+
+routers="      tls:
+        certresolver: $CERT_TYPE
+"
+
+certResolvers="
 certificatesResolvers:
   staging:
     acme:
       email: no-reply@stranerd.com
       storage: $CERT_PATH_STAGING
-      caServer: "https://acme-staging-v02.api.letsencrypt.org/directory"
+      caServer: \"https://acme-staging-v02.api.letsencrypt.org/directory\"
       httpChallenge:
         entryPoint: web
 
@@ -63,46 +70,15 @@ certificatesResolvers:
     acme:
       email: no-reply@stranerd.com
       storage: $CERT_PATH_PRODUCTION
-      caServer: "https://acme-v02.api.letsencrypt.org/directory"
+      caServer: \"https://acme-v02.api.letsencrypt.org/directory\"
       httpChallenge:
         entryPoint: web
-EOF
-else
-cat > $CONF_PATH <<- EOF
-global:
-  checkNewVersion: true
-  sendAnonymousUsage: false
-
-entryPoints:
-  web:
-    address: :80
-
-http:
-  middlewares:
-    stripRoutePrefix:
-      stripPrefix:
-        prefixes:
-          - "/api"
-  routers:
-    api:
-      rule: "PathPrefix(\`/api/\`)"
-      middlewares:
-        - stripRoutePrefix
-      service: api
-
-  services:
-    api:
-      loadBalancer:
-        servers:
-          - url: http://api:$PORT/
-
-api:
-  insecure: true
-  dashboard: true
-
-providers:
-  file:
-    directory: /etc/traefik
-    watch: true
-EOF
+"
 fi
+
+cat > /etc/traefik/traefik.yml <<- EOF
+$similar
+$routers
+$entryPoints
+$certResolvers
+EOF
