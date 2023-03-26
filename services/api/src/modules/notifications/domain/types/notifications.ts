441104fc892c82874ec0e5aab531dcac9bb80366
kevinand11 <kevinfizu@gmail.com>
2023-03-26 14:00:19 +0100

    feat: send notification after being verified

--- services/api/src/modules/notifications/domain/types/notifications.ts
+++ services/api/src/modules/notifications/domain/types/notifications.ts
@@ -2,9 +2,13 @@ export enum NotificationType {
 	ConnectRequested = 'ConnectRequested',
 	ConnectAccepted = 'ConnectAccepted',
 	ConnectDeclined = 'ConnectDeclined',
+	VerificationAccepted = 'VerificationAccepted',
+	VerificationRejected = 'VerificationRejected',
 }
 
 export type NotificationData =
 	{ type: NotificationType.ConnectRequested, connectId: string, userId: string }
 	| { type: NotificationType.ConnectAccepted, connectId: string, userId: string }
 	| { type: NotificationType.ConnectDeclined, connectId: string, userId: string }
+	| { type: NotificationType.VerificationAccepted, verificationId: string }
+	| { type: NotificationType.VerificationRejected, verificationId: string }
--- services/api/src/modules/users/utils/changes/verifications.ts
+++ services/api/src/modules/users/utils/changes/verifications.ts
@@ -1,5 +1,7 @@
+import { AuthUsersUseCases } from '@modules/auth'
+import { NotificationType, sendNotification } from '@modules/notifications'
 import { appInstance } from '@utils/types'
-import { DbChangeCallbacks } from 'equipped'
+import { AuthRole, DbChangeCallbacks } from 'equipped'
 import { VerificationFromModel } from '../../data/models/verifications'
 import { VerificationEntity } from '../../domain/entities/verifications'
 
@@ -10,11 +12,27 @@ export const VerificationDbChangeCallbacks: DbChangeCallbacks<VerificationFromMo
 			`users/verifications/${after.userId}`, `users/verifications/${after.id}/${after.userId}}`,
 		], after)
 	},
-	updated: async ({ after }) => {
+	updated: async ({ after, before, changes }) => {
 		await appInstance.listener.created([
 			'users/verifications', `users/verifications/${after.id}`,
 			`users/verifications/${after.userId}`, `users/verifications/${after.id}/${after.userId}}`,
 		], after)
+
+		if (changes.pending && before.pending && !after.pending) {
+			if (after.accepted) await AuthUsersUseCases.updateUserRole({
+				userId: after.userId, roles: { [AuthRole.isVerified]: true }
+			})
+
+			await sendNotification([after.userId], {
+				title: 'Verification Status',
+				body: `Your verification request has been ${after.accepted ? 'accepted' : 'rejected'}`,
+				sendEmail: true,
+				data: {
+					type: after.accepted ? NotificationType.VerificationAccepted : NotificationType.VerificationRejected,
+					verificationId: after.id
+				}
+			})
+		}
 	},
 	deleted: async ({ before }) => {
 		await appInstance.listener.created([
