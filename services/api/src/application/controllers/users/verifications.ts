7bd7a3886921ac5ca2f9c93107973943881e3ad4
kevinand11 <kevinfizu@gmail.com>
2023-03-26 13:47:11 +0100

    feat: verify content before creating verification

--- services/api/src/application/controllers/users/verifications.ts
+++ services/api/src/application/controllers/users/verifications.ts
@@ -1,6 +1,9 @@
+import { AuthUsersUseCases } from '@modules/auth'
+import { CoursesUseCases, DraftStatus, QuizzesUseCases } from '@modules/study'
 import { VerificationSocials, VerificationsUseCases } from '@modules/users'
 import {
 	BadRequestError,
+	Conditions,
 	NotAuthorizedError,
 	QueryParams,
 	Request,
@@ -43,7 +46,12 @@ export class VerificationsController {
 			}),
 		}, req.body)
 
-		// TODO; verify ownership of contents and profile
+		const user = await AuthUsersUseCases.findUser(authUser.id)
+		if (!user) throw new BadRequestError('User not found')
+		if (!user.photo || !user.description) throw new BadRequestError('Complete your bio before applying for verification')
+
+		const validContent = await this.verifyContent(authUser.id, content)
+		if (!validContent) throw new BadRequestError('Make sure all content provided belong to you and are published')
 
 		return await VerificationsUseCases.create({
 			userId: authUser.id, socials, content,
@@ -58,5 +66,27 @@ export class VerificationsController {
 		const isUpdated = await VerificationsUseCases.accept({ id: req.params.id, accept })
 		if (isUpdated) return isUpdated
 		throw new NotAuthorizedError()
+	}
+
+	private static async verifyContent (userId: string, content: Parameters<typeof VerificationsUseCases.create>[0]['content']) {
+		const { results: courses } = await CoursesUseCases.get({
+			where: [{ field: 'id', value: content.courses, condition: Conditions.in }],
+			all: true
+		})
+
+		if (courses.length !== content.courses.length) return false
+		const validCourses = courses.every((course) => course.user.id === userId && course.status === DraftStatus.published)
+		if (!validCourses) return false
+
+		const { results: quizzes } = await QuizzesUseCases.get({
+			where: [{ field: 'id', value: content.quizzes, condition: Conditions.in }],
+			all: true
+		})
+
+		if (quizzes.length !== content.quizzes.length) return false
+		const validQuizzes = quizzes.every((quiz) => quiz.user.id === userId && quiz.status === DraftStatus.published && !quiz.courseId)
+		if (!validQuizzes) return false
+
+		return true
 	}
 }
