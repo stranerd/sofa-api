1783305040f9e449e71cdf4c0bf44647f80d7db9
kevinand11 <kevinfizu@gmail.com>
2023-04-25 23:36:33 +0100

    fix: use google storage only in prod

--- services/api/src/modules/storage/index.ts
+++ services/api/src/modules/storage/index.ts
@@ -1,8 +1,8 @@
-import { isDev } from '@utils/environment'
+import { isProd } from '@utils/environment'
 import { CloudUploaderRepository } from './data/repositories/cloudUploader'
 import { LocalUploaderRepository } from './data/repositories/localUploader'
 import { UploaderUseCase } from './domain/useCases/uploader'
 
-const uploaderRepository = isDev ? new LocalUploaderRepository() : new CloudUploaderRepository()
+const uploaderRepository = isProd ? new CloudUploaderRepository() : new LocalUploaderRepository()
 
 export const UploaderUseCases = new UploaderUseCase(uploaderRepository)
