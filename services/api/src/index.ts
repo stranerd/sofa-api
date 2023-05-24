f4fe556bec494de2a638df3cd3144d40c208b286
kevinand11 <kevinfizu@gmail.com>
2023-05-24 10:06:56 +0100

    Revert "refactor:  initialize app only if not dev"
    This reverts commit b608163730405d264ecdeaad4800f41e9ec9a012.

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,14 +1,14 @@
 import { routes } from '@application/routes'
 import { PlansUseCases } from '@modules/payment'
 import { UsersUseCases } from '@modules/users'
-import { isDev, port } from '@utils/environment'
+import { isProd, port } from '@utils/environment'
 import { startJobs } from '@utils/jobs'
 import { registerSockets } from '@utils/sockets'
 import { appInstance } from '@utils/types'
 import { initializeApp } from 'firebase-admin/app'
 
 const start = async () => {
-	if (!isDev) initializeApp()
+	if (isProd) initializeApp()
 	await appInstance.startConnections()
 
 	await registerSockets()
