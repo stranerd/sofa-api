e04f7fcb97c411477fe87f4098019c222cee7b18
kevinand11 <kevinfizu@gmail.com>
2023-08-16 14:45:48 +0100

    chore: update deps

--- services/api/src/modules/notifications/utils/push.ts
+++ services/api/src/modules/notifications/utils/push.ts
@@ -1,22 +1,18 @@
 import { appInstance, PushNotification } from '@utils/types'
 import { messaging } from 'firebase-admin'
 import { TokensUseCases } from '../'
-
-const chunkArray = <T> (arr: T[], size: number) => new Array(Math.ceil(arr.length / size))
-	.fill([])
-	.map((_, index) => arr.slice(index * size, (index + 1) * size))
-	.filter((chunk) => chunk.length > 0)
+import { Validation } from 'equipped'
 
 export const sendPushNotification = async (notification: PushNotification) => {
 	try {
 		const { userIds, data, title, body } = notification
 		await Promise.all(userIds.map(async (userId) => {
 			const userTokens = await TokensUseCases.find({ userId })
-			const chunks = chunkArray(userTokens.tokens, 500)
+			const chunks = Validation.chunkArray(userTokens.tokens, 500)
 			const invalidTokens = [] as string[]
 
 			await Promise.all(chunks.map(async (tokens) => {
-				const res = await messaging().sendMulticast({
+				const res = await messaging().sendEachForMulticast({
 					tokens,
 					notification: { title, body },
 					data: { value: JSON.stringify(data) }
