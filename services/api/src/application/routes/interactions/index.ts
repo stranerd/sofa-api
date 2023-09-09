37b3492a24aa860a5a1a036203928e3888b4d7ed
kevinand11 <kevinfizu@gmail.com>
2023-09-10 00:59:34 +0100

    feat: reports routes

--- services/api/src/application/routes/interactions/index.ts
+++ services/api/src/application/routes/interactions/index.ts
@@ -1,6 +1,7 @@
 import { groupRoutes } from 'equipped'
 import { commentsRoutes } from './comments'
 import { likesRoutes } from './likes'
+import { reportsRoutes } from './reports'
 import { reviewsRoutes } from './reviews'
 import { tagsRoutes } from './tags'
 import { viewsRoutes } from './views'
@@ -8,6 +9,7 @@ import { viewsRoutes } from './views'
 export const interactionRoutes = groupRoutes('/interactions', [
 	...commentsRoutes,
 	...likesRoutes,
+	...reportsRoutes,
 	...reviewsRoutes,
 	...tagsRoutes,
 	...viewsRoutes
---
+++ services/api/src/application/routes/interactions/reports.ts
@@ -0,0 +1,58 @@
+import { ReportController } from '@application/controllers/interactions/reports'
+import { groupRoutes, makeController, StatusCodes } from 'equipped'
+import { isAdmin, isAuthenticated } from '@application/middlewares'
+
+export const reportsRoutes = groupRoutes('/reports', [
+	{
+		path: '/',
+		method: 'get',
+		controllers: [
+			isAuthenticated, isAdmin,
+			makeController(async (req) => {
+				return {
+					status: StatusCodes.Ok,
+					result: await ReportController.get(req)
+				}
+			})
+		]
+	},
+	{
+		path: '/:id',
+		method: 'get',
+		controllers: [
+			isAuthenticated, isAdmin,
+			makeController(async (req) => {
+				return {
+					status: StatusCodes.Ok,
+					result: await ReportController.find(req)
+				}
+			})
+		]
+	},
+	{
+		path: '/:id',
+		method: 'delete',
+		controllers: [
+			isAuthenticated, isAdmin,
+			makeController(async (req) => {
+				return {
+					status: StatusCodes.Ok,
+					result: await ReportController.delete(req)
+				}
+			})
+		]
+	},
+	{
+		path: '/',
+		method: 'post',
+		controllers: [
+			isAuthenticated,
+			makeController(async (req) => {
+				return {
+					status: StatusCodes.Ok,
+					result: await ReportController.create(req)
+				}
+			})
+		]
+	}
+])
