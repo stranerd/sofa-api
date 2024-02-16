45676026ae9790f950555a4fecd8c37db3f12a6a
kevinand11 <kevinfizu@gmail.com>
2024-02-16 01:02:52 +0100

    fix: debugging create users orgsIn

--- services/api/src/modules/users/data/repositories/users.ts
+++ services/api/src/modules/users/data/repositories/users.ts
@@ -41,7 +41,7 @@ export class UserRepository implements IUserRepository {
 				$set: { bio: data },
 				$setOnInsert: { _id: userId, dates: { createdAt: timestamp, deletedAt: null } },
 			},
-			{ upsert: true },
+			{ upsert: true, new: true },
 		)
 	}
 
@@ -56,7 +56,7 @@ export class UserRepository implements IUserRepository {
 			{
 				$set: { 'dates.deletedAt': timestamp },
 			},
-			{ upsert: true },
+			{ upsert: true, new: true },
 		)
 	}
 
@@ -87,7 +87,7 @@ export class UserRepository implements IUserRepository {
 			{
 				$set: { roles: data },
 			},
-			{ upsert: true },
+			{ upsert: true, new: true },
 		)
 	}
 
