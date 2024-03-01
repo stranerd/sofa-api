02a7cce2668846fc525fe25256dd096ae2cf14e6
kevinand11 <kevinfizu@gmail.com>
2024-03-01 21:21:15 +0100

    fix: add validation for meta phone number

--- services/api/src/application/controllers/meta/messages.ts
+++ services/api/src/application/controllers/meta/messages.ts
@@ -1,5 +1,5 @@
 import { publishers } from '@utils/events'
-import { EmailsList, Request, Schema, readEmailFromPug, validate } from 'equipped'
+import { EmailsList, Request, Schema, readEmailFromPug, validate, Validation } from 'equipped'
 
 export class MessageController {
 	static async createMessage(req: Request) {
@@ -7,13 +7,16 @@ export class MessageController {
 			{
 				name: Schema.string(),
 				email: Schema.string().email(),
-				phone: Schema.string(),
+				phone: Schema.any().addRule(Validation.isValidPhone()),
 				message: Schema.string().min(1),
 			},
 			req.body,
 		)
 
-		const content = await readEmailFromPug('emails/newFormMessage.pug', data)
+		const content = await readEmailFromPug('emails/newFormMessage.pug', {
+			...data,
+			phone: `${data.phone.code}${data.phone.number}`,
+		})
 		await publishers.SENDMAIL.publish({
 			from: EmailsList.NO_REPLY,
 			to: 'support@stranerd.com',
