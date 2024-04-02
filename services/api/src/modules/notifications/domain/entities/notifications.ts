28b180a3be91183f4b157604b5aeb958d0418a86
kevinand11 <kevinfizu@gmail.com>
2024-04-02 01:50:22 +0100

    refactor: notifications link

--- services/api/src/modules/notifications/domain/entities/notifications.ts
+++ services/api/src/modules/notifications/domain/entities/notifications.ts
@@ -1,3 +1,4 @@
+import { Purchasables } from '@modules/payment'
 import { BaseEntity } from 'equipped'
 import { NotificationData, NotificationType } from '../types'
 
@@ -14,9 +15,10 @@ export class NotificationEntity extends BaseEntity<NotificationConstructorArgs> 
 		else if (not.type === NotificationType.TutorRequestAccepted) return '/settings/profile'
 		else if (not.type === NotificationType.TutorRequestRejected) return '/settings/profile'
 		else if (not.type === NotificationType.NewPurchase) return '/settings/wallet'
-		else if (not.type === NotificationType.NewPurchased) return `/${not.purchasedType}/${not.purchasedId}`
-		else if (not.type === NotificationType.TutorAddedToConversation) return `/chats/${not.conversationId}`
-		else if (not.type === NotificationType.UserJoinedGame) return `/games/${not.gameId}/lobby`
+		else if (not.type === NotificationType.NewPurchased) {
+			if (not.purchasedType === Purchasables.courses) return `/study/courses/${not.purchasedId}`
+		} else if (not.type === NotificationType.TutorAddedToConversation) return `/chats/${not.conversationId}`
+		else if (not.type === NotificationType.UserJoinedGame) return `/plays/games/${not.gameId}/lobby`
 		else if (not.type === NotificationType.WithdrawalSuccessful) return '/settings/wallet'
 		else if (not.type === NotificationType.WithdrawalFailed) return '/settings/wallet'
 		else if (not.type === NotificationType.WalletFundSuccessful) return '/settings/wallet'
@@ -24,11 +26,11 @@ export class NotificationEntity extends BaseEntity<NotificationConstructorArgs> 
 		else if (not.type === NotificationType.SubscriptionFailed) return '/settings/subscription'
 		else if (not.type === NotificationType.GenericSubscriptionSuccessful) return '/settings/wallet'
 		else if (not.type === NotificationType.GenericSubscriptionFailed) return '/settings/wallet'
-		else if (not.type === NotificationType.NewQuizAccessRequest) return `/quiz/${not.quizId}/edit`
-		else if (not.type === NotificationType.QuizAccessRequestGranted) return `/quiz/${not.quizId}/edit`
-		else if (not.type === NotificationType.QuizAccessRequestRejected) return `/quiz/${not.quizId}`
-		else if (not.type === NotificationType.QuizAccessMemberGranted) return `/quiz/${not.quizId}/edit`
-		else if (not.type === NotificationType.QuizAccessMemberRebuked) return `/quiz/${not.quizId}`
+		else if (not.type === NotificationType.NewQuizAccessRequest) return `/study/quizzes/${not.quizId}/edit`
+		else if (not.type === NotificationType.QuizAccessRequestGranted) return `/study/quizzes/${not.quizId}/edit`
+		else if (not.type === NotificationType.QuizAccessRequestRejected) return `/study/quizzes/${not.quizId}`
+		else if (not.type === NotificationType.QuizAccessMemberGranted) return `/study/quizzes/${not.quizId}/edit`
+		else if (not.type === NotificationType.QuizAccessMemberRebuked) return `/study/quizzes/${not.quizId}`
 
 		return '/dashboard'
 	}
