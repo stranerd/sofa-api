9d77b9598c01c6649431b3da9a1ba03d9efe5822
kevinand11 <kevinfizu@gmail.com>
2024-04-20 11:18:37 +0100

    Revert "feat: fix courses sections route"
    This reverts commit 807e7d08e1c34149ee93dcffe9492ddebc736125.

--- services/api/src/application/routes/study/courses.ts
+++ services/api/src/application/routes/study/courses.ts
@@ -2,48 +2,11 @@ import { CourseController } from '@application/controllers/study/courses'
 import { isAuthenticated } from '@application/middlewares'
 import { groupRoutes, makeController } from 'equipped'
 
-import { Coursable, QuizModes } from '@modules/study'
-import { Course } from '@modules/study/data/mongooseModels/courses'
-import { File } from '@modules/study/data/mongooseModels/files'
-
 export const coursesRoutes = groupRoutes('/courses', [
 	{
 		path: '/',
 		method: 'get',
 		controllers: [makeController(async (req) => CourseController.get(req))],
-	},
-	{
-		path: '/fix-sections',
-		method: 'get',
-		controllers: [
-			makeController(async () => {
-				let result: any
-				await Course.collection.conn.transaction(async (session) => {
-					const allCourses = await Course.find({}, null, { session })
-					const allFiles = await File.find({}, null, { session })
-					const filesMap = Object.fromEntries(allFiles.map((file) => [file._id.toString(), file]))
-
-					const bulk = Course.collection.initializeUnorderedBulkOp()
-					for (const course of allCourses) {
-						const sections = course.sections.map((section) => ({
-							...section,
-							items: section.items.map((item) => {
-								if (['fileType', 'quizMode'].some((key) => key in item)) return item
-								if (item.type === Coursable.file && filesMap[item.id]) return { ...item, fileType: filesMap[item.id].type }
-								if (item.type === Coursable.quiz) return { ...item, quizMode: QuizModes.practice }
-								return item
-							}),
-						}))
-						bulk.find({ _id: course._id }).updateOne({ $set: { sections } })
-					}
-					result = await bulk.execute({ session })
-				})
-				return {
-					result,
-					courses: await Course.find({}),
-				}
-			}),
-		],
 	},
 	{
 		path: '/:id',
